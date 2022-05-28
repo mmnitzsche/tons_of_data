1ca672be6ce28d3b42f43d4fa6fc8b3785e37294
mmnitzsche <mmnitzsche@gmail.com>
2022-05-28 05:05:44 -0300

    correção da importação dos países

--- data_gen.py
+++ data_gen.py
@@ -1,37 +1,37 @@
-#%%
+# %%
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
-from random import random
+import random
 from datetime import datetime, timedelta, date
 
-# v22222222222
 
 def get_countries():
-    global country
-    global region
-    global gdp
-    global data
 
     url = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     countries = []
-    # table = soup.find('table', class_='wikitable').find_all('tr')
+
     for items in soup.find('table', class_='wikitable').find_all('tr'):
         data = items.find_all(['td'])
-        print(data)
         try:
             country = data[0].a.text
             region = data[1].a.text
             gdp = "".join(filter(str.isdigit,data[2].text))
             gdp = data[2].text
-            
-        except: IndexError
-        countries.append({'country': country,'region': region, 'gdp': gdp})
+        except:
+            pass
+
+        try:
+            countries.append({'country': country,'region': region, 'gdp': gdp})
+        except:
+            pass
+
     return countries
+
 
 def cleaning_countries(df):
     df_countries = pd.DataFrame(df)
@@ -40,6 +40,7 @@ def cleaning_countries(df):
     df_countries['gdp'] = df_countries['gdp'].astype(int)
     df_countries  = df_countries.sort_values(by='gdp', ascending=False).head(15)
     return df_countries
+
 
 def get_companies():
     companies = []
@@ -53,36 +54,84 @@ def get_companies():
     choosed_companies = companies[:13]
     return choosed_companies
 
+
+clothes_list = [
+'T-shirt',
+'Topics',
+'Twitter',
+'Videos',
+'bag',
+'belt',
+'blouse',
+'boots',
+'bow-tie',
+'bra',
+'bracelet',
+'briefs',
+'cap',
+'coat',
+'dress',
+'earrings',
+'glasses',
+'handkerchief',
+'hat ',
+'heels',
+'jacket',
+'jeans',
+'necklace',
+'pajamas',
+'panties',
+'performance',
+'preferences',
+'purse',
+'raincoat',
+'ring',
+'scarf',
+'shawl',
+'shirt',
+'shoes',
+'shorts',
+'skirt',
+'socks',
+'stockings',
+'suit',
+'sun hat',
+'sunglasses',
+'sweater',
+'swimsuit',
+'tanktop',
+'tie',
+'tracksuit',
+'vest',
+'wallet',
+'watch',
+'wool hat']
+
+
 countries = get_countries()
 df_countries = cleaning_countries(df=countries)
 brand_list = get_companies()
-countries_values = df_countries['country'].tolist()
+countries_values = list(df_countries['country'].tolist())
 
-
-# teste commmit
-
-# %%
 
 c_list = []
+
 for c in range(90):
     country = random.choice(countries_values)
     companies = random.choice(brand_list)
-    c_list.append({'country':country,'companies':companies})
-    # print(c_list)
+    clothes = random.choice(clothes_list)
+    clothes = random.choice(clothes_list)
+    clothes = random.choice(clothes_list)
 
-# c_list
+
+    c_list.append({'country':country,'companies':companies,'clothes':clothes})
+
 
 df = pd.DataFrame(c_list)
-df.value_counts()
+df
   
 
+  
 # %%
 
-url = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
 
-for items in soup.find('table', class_='wikitable').find_all('tr','a'):
-    data = items.find_all(['td'])
-    print(data[0].text)
-print(data[0].text)
